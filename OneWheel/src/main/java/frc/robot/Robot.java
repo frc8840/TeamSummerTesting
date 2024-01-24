d08be82fa09e0d20005b0c79e6b83b176a60a5a8
thebayrobotics <frc8840@gmail.com>
2024-01-23 18:22:52 -0800

    Simulated driving is now in networktables. Code is still sloppy, but it works.

--- OneWheel/src/main/java/frc/robot/Robot.java
+++ OneWheel/src/main/java/frc/robot/Robot.java
@@ -6,10 +6,15 @@ package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.WPILibVersion;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.DoubleArrayPublisher;
@@ -34,6 +39,8 @@ public class Robot extends TimedRobot {
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
   double[] testValue = {0, 0};
+  Pose2d testPosition = new Pose2d();
+  private final String WPILibVer = new WPILibVersion().Version;
 
   @Override
   public void robotInit() {
@@ -86,12 +93,17 @@ public class Robot extends TimedRobot {
             * Drivetrain.kMaxAngularSpeed;
 
     m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
-    double[] position = {xSpeed, ySpeed, rot};
+    double[] position = {xSpeed, ySpeed, rot / 20.0};
     testValue[0] += 1/60.0;
     if (testValue[0] > 2*Math.PI) testValue[0] = 0;
     testValue[1] = Math.cos(testValue[0]);
     posePub.set(testValue);
-    posePub2.set(position);
+    // x = position[0]
+    // y = position[1]
+    // theta = position[2]
+    testPosition = testPosition.transformBy(new Transform2d(new Translation2d(position[0], position[1]), new Rotation2d(position[2])));
+    double[] toPublish = {testPosition.getX(), testPosition.getY(), testPosition.getRotation().getRadians()};
+    posePub2.set(toPublish);
     
   }
 }
